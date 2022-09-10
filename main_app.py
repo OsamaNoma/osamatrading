4ee693050c692e661b6d51b3f014d6ee3cf5935b
OsamaNoma <89946466+OsamaNoma@users.noreply.github.com>
2022-09-10 16:20:51 +0300

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -8,6 +8,18 @@ import requests
 
 while True:
 
+ Pkey = 'wPwRh8oHCGhpLMF9zdYRWFrHxniROVX2A9XQmLTnIFbVCqv8asr9rnFuVvcf7vlM'
+ Skey = 'KqgHRiQfZJloSG4ZId7ahPiH62Ql6q5PovGJwuJ8m24kL1QOF8qCmRswnDdaCxTZ'
+
+ client = Client(api_key=Pkey, api_secret=Skey) 
+
+ base_url = "https://api.binance.com"
+ endpoint = "/api/v1/ping"
+ Binance = base_url + endpoint
+
+ r = requests.get(Binance)
+ print(r.json(),"Binance server is : Connected"),Tb.send_message("Binance server is : Connected")
+
  def time_now():
   time = dt.datetime.now()
   time = time.strftime("%H:%M:%S    //   %d-%m-%Y") #10:42:30   //   01-03-2021
